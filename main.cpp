50bed6858993d08a65b723d714019b161177b07b
Zephan <number45zephan@gmail.com>
2018-03-28 08:10:24 -0400

    Netbeans Project Creation
    Added an example of how to output timing wise, and created most of the neccessary functions.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+// usleep(500); ---> for linux
+#include <Windows.h>
+
+using namespace std;
+
+const int easy = 1000;
+const int medium = 500;
+const int hard = 250;
+
+int main() {
+    int time;
+    
+    cout << "Hello World!" << endl;
+    Sleep(1000);
+//    system ("CLS");
+    system("clear");
+    cout << "Goodbye World!" << endl;
+    
+    return 0;
+}
+
+void printTable(){
+
+}
+void printRed(int t){
+
+}
+void printGreen(int t){
+
+}
+void printBlue(int t){
+
+}
+void Yellow(int t){
+
+}
